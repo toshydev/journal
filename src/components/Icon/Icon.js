27355ba4f01b11918be1c35bad871d953041c627
Anton Roters <antonroters@hotmail.com>
2023-03-08 22:33:43 +0100

    improve svg icons/button size/input style

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,29 +1,15 @@
 import "./Icon.css";
+import { ReactComponent as StarFilled } from "../../assets/images/star-filled.svg";
+import { ReactComponent as Star } from "../../assets/images/star.svg";
 
 export default function Icon({ active }) {
   return (
-    <a className="icon-button" aria-label="favorite button">
-      <svg>
-        {active ? (
-          <path
-            d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
-            fill="#FF4A11"
-            stroke="#FF4A11"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        ) : (
-          <path
-            d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
-            fill="none"
-            stroke="#252629"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        )}
-      </svg>
-    </a>
+    <button className="icon-button" aria-label="favorite button" type="button">
+      {active ? (
+        <StarFilled title="marked as favorite" />
+      ) : (
+        <Star title="not marked as favorite" />
+      )}
+    </button>
   );
 }
